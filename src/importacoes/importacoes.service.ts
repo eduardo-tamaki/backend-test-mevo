592cd0d3ab698fb1b5e9dbbb991c52617ef5442b
Fernando Custodio <fwcustodio@gmail.com>
2024-08-27 16:20:57 -0400

    Commit contabilizando as operações, e separando por validas ou nao. Proximo passo adicionar ao banco de dados

--- src/importacoes/importacoes.service.ts
+++ src/importacoes/importacoes.service.ts
@@ -9,18 +9,69 @@ export class ImportacoesService {
   create(FileCVS: Express.Multer.File) {}
 
   async importarArquivo(FileCVS: Express.Multer.File) {
-    const results = [];
-
+    const RegistrosCSV = [];
     const stream = Readable.from(FileCVS.buffer).pipe(csv());
 
     // Parse o CSV linha a linha
     stream.on('data', (row) => {
-      results.push(row);
+      RegistrosCSV.push(row);
     });
 
-    stream.on('end', () => {
-      console.log(results); // Aqui você pode trabalhar com os dados lidos
-    }); //
+    stream.on('end', processarArquivos); //
+
+    async function processarArquivos() {
+      let RegistrosIncluir = [];
+      let RegistrosDuplicados = [];
+      let RegistrosNegativos = [];
+
+      for (let index = 0; index < RegistrosCSV.length; index++) {
+        const Item = RegistrosCSV[index];
+
+        const Colunas = Item['from;to;amount'];
+        //console.log(Colunas);
+
+        const ArrayColunas = Colunas.split(';');
+
+        let From = ArrayColunas[0];
+        let To = ArrayColunas[1];
+        let Amount = ArrayColunas[2];
+        let ID = From + To + Amount;
+        Amount = parseFloat(Amount) / 100;
+
+        if (Amount < 0) {
+          RegistrosNegativos.push({
+            from: From,
+            to: To,
+            amount: Amount,
+            id: ID,
+          });
+          continue;
+        }
+
+        if (RegistrosIncluir.find((x) => x.id === ID)) {
+          //evita duplicação
+          RegistrosDuplicados.push({
+            from: From,
+            to: To,
+            amount: Amount,
+            id: ID,
+          });
+          continue;
+        }
+
+        RegistrosIncluir.push({
+          from: From,
+          to: To,
+          amount: Amount,
+          id: ID,
+          operacao_suspeita: Amount > 50000,
+        });
+      }
+
+      console.log('RegistrosDuplicados', JSON.stringify(RegistrosDuplicados));
+      console.log('RegistrosNegativos', JSON.stringify(RegistrosNegativos));
+      console.log('RegistrosIncluir', JSON.stringify(RegistrosIncluir));
+    }
   }
 
   findAll() {
