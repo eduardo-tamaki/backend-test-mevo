39f3301843be51a68bcfbe0e864ca540c6bea6ce
magoterra <marcielis@xgrow.com>
2024-10-31 20:17:52 -0300

    feat: csvProcess controller

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    await this.appService.processCsv(file);
+  }
+}
