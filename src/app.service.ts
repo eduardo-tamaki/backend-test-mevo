dc64e6de1f73feb6a35bb5d45278c78622a06d60
magoterra <marcielis@xgrow.com>
2024-10-31 21:18:05 -0300

    feat: csvProcess service_pt1

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,71 @@
 import { Injectable } from '@nestjs/common';
+import { validate } from 'class-validator';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async processCsv(file: Express.Multer.File): Promise<any> {
+    const bufferFile = file.buffer;
+
+    const parseData: any = await new Promise((resolve, reject) => {
+      csv.parser(
+        bufferFile,
+        {
+          columns: true,
+          relax_quotes: true,
+          skip_empty_lines: true,
+          cast: true,
+        },
+        (error, records) => {
+          if (error) {
+            reject(error);
+            return {
+              error: true,
+              message: 'Invalido para parse',
+            };
+          }
+          resolve(records);
+        },
+      );
+
+      const errors: string[] = [];
+      if (!parseData.length) {
+        errors.push('Arquivo vazio');
+        return {
+          error: true,
+          message: 'arquivo invalido',
+          errorsArray: errors,
+        };
+      }
+
+      for await (const [index, rowData] of parseData.entries()) {
+        const validationErros = await this.validateFileRow(rowData);
+
+        if (validationErros.length) {
+          return {
+            error: true,
+            errorsArray: validationErros,
+            message: 'Falha validação linha:' + (index + 1),
+          };
+        }
+      }
+
+      return {
+        errors: false,
+      };
+    });
   }
 
-  async processCsv(file: Express.Multer.File) {
-    console.log(file);
+  async validateFileRow(rowData) {
+    const erros: string[] = [];
+    const csvDto = plainToInstance(CSVDTO, rowData);
+    const validationErros = await validate(csvDto);
+
+    if (validationErros.length > 0) {
+      validationErros.forEach((error) => {
+        const { property, constraints } = error;
+        const errorMessage = property + constraints.join(', ');
+        erros.push(errorMessage);
+      });
+    }
   }
 }
